27341ba405622ac277d90c8a28c65589b12de430
smiteshz <smiteshsawantz@gmail.com>
2018-03-17 22:45:00 -0700

    Fixed issue with the POST request handling
    Switched the positions of body parser middleware.

--- server.js
+++ server.js
@@ -5,10 +5,12 @@ const UM = require('./userManagement/user.js');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
 
 app.post('/register/', UM.register);
 
+app.get('/users/', UM.displayList);
 
 app.listen(4000);
--- userManagement/user.js
+++ userManagement/user.js
@@ -3,13 +3,23 @@ const mongoose = require('mongoose');
 let userModel = mongoose.model("user");
 
 module.exports.register = (req, res) => {
+    console.log(`Here's the request body: `);
     console.log(req.body);
     newUser = new userModel(
         req.body
     );
-    newUser.save().then(result => {console.log(result)}).catch(
+    newUser.save().then(result => {
+        console.log(result);
+        res.json({success: true});
+    }).catch(
         err => {
             if (err) console.log(err);
+            res.json({success: false})
         }
     )
+    
+};
+
+module.exports.displayList = (req, res) => {
+    
 };
